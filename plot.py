fc784f7b8d591416f4063b53b1fe34fe6a793145
Kavyarustagi12 <kavyarustagi00@gmail.com>
2026-03-21 08:16:28 +0530

    assets

--- plot.py
+++ plot.py
@@ -14,7 +14,9 @@ with open(s) as file:
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 ax.scatter(x, y, z, s=2,color="green")
 
 plt.show()
