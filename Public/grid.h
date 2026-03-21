cb4e97a7bc78e44438c3129b58536b6587881680
Kavyarustagi12 <kavyarustagi00@gmail.com>
2026-03-21 13:24:16 +0530

    a more detailed explaination

--- Public/grid.h
+++ Public/grid.h
@@ -13,7 +13,7 @@ public:
     float voxel_size;
     std::vector<float> model; // a 1 d array storing all the 3d coordinates
 
-    Grid(int s, float vs); /
+    Grid(int s, float vs); 
 
     void sample(DefineClass& shape); 
     void exportf(const std::string& filename);
